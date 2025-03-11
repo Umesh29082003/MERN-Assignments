1a66f8e282ecd4ad4de78a317955106d5a42cb81
Umesh29082003 <sahoou844@gmail.com>
2025-03-11 17:30:22 +0530

    Commit

--- Week_5/p77.js
+++ Week_5/p77.js
@@ -3,4 +3,4 @@ function func(arr)
     return arr.length==2 && (arr[0]==1||arr[0]==3||arr[1]==1||arr[1]==3)
 }
 
-console.log(func())
+console.log(func([2,3]))
---
+++ Week_5/p78.js
@@ -0,0 +1,6 @@
+function func(arr)
+{
+    return arr.length==2 && (arr[0]!=1 && arr[0]!=3 && arr[1]!=1 && arr[1]!=3)
+}
+
+console.log(func([2,3]))
